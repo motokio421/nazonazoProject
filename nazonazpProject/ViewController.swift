7f5cf59d4e81a15cc3996b2e02818d518f6b5c5b
motoki <higamotoki5731@gmail.com>
2019-08-08 20:08:39 +0800

    first comit

---
+++ nazonazpProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nazonazpProject
+//
+//  Created by 比嘉幹樹 on 2019/08/08.
+//  Copyright © 2019 比嘉幹樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
